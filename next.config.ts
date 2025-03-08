3d999af89f2f34630df726b33bf65bca7c87d8a0
mohak1712 <mohakpuri1712@gmail.com>
2025-03-08 17:33:44 +0530

    updates

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    reactStrictMode: true,
+    output: "export", // Required for Netlify
+    trailingSlash: true, // Fixes static routing issues
 };
 
 export default nextConfig;
